f432b73b7a1228eeba62ea14f0cf89adcd7553c6
Amit Jaison <127863443+Amitjaison31@users.noreply.github.com>
2023-09-08 17:03:06 +0530

    Update and rename Solution.java to AddDigits.java

---
+++ AddDigits.java
@@ -0,0 +1,25 @@
+public class AddDigits {
+	
+	    public int addDigits(int num) {
+	        while (num>9){
+	            int sum = 0;
+	            while (num>0){
+	                sum = sum + (num%10) ;
+	                num = num / 10;
+	            }
+	            num = sum;
+	        }
+	        return num;
+	    }
+	
+
+	public static void main(String[] args) {
+		System.out.println("Enter the digits: ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		AddDigits add = new AddDigits();
+		System.out.println("The sum of the digits till the digit is single digit: "+add.addDigits(num));
+		
+	}
+
+}
