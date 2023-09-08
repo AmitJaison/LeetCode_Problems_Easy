79085af3f8e3dadf31dd2ca73f34bbe17efcecd0
Amit Jaison <127863443+Amitjaison31@users.noreply.github.com>
2023-09-08 17:03:52 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,1 @@
-public static void main(String[] args) {
-		System.out.println("Enter the digits: ");
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		AddDigits add = new AddDigits();
-		System.out.println("The sum of the digits till the digit is single digit: "+add.addDigits(num));
-		
-	}
+
